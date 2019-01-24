718533ac377bc7eb4e5432b0104e430770d225ee
Jin-12 <coutjules@free.fr>
2019-01-24 13:31:46 +0100

    board and show init

---
+++ lib/board.rb
@@ -0,0 +1,10 @@
+class Board
+
+def initialize
+  @empty_case = ['         ', '         ', '         ']
+  @player_1_case = ['   OOO   ','  O   O  ','   OOO   ']
+  @player_2_case = ['  XX XX  ','   XXX   ','  XX XX  ']
+  @horizontal_separator = ['---------']
+end
+
+end
---
+++ lib/show.rb
@@ -0,0 +1,3 @@
+class Show
+
+end
