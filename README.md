833c7645fc86c7b404c2699a2a7ce892ba07afad
Jin-12 <coutjules@free.fr>
2019-01-24 10:44:27 +0100

    initialization

---
+++ README.md
@@ -0,0 +1,2 @@
+Hello everybody!
+This program you're looking at is a Ruby program.
---
+++ app.rb
@@ -0,0 +1,4 @@
+require 'bundler'
+Bundler.require
+require 'scrapper'
+$:.unshift File.expand_path('./../lib', __FILE__)
